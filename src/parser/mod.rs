ee966b365f8adb866bf1be6ff0207738508a8cc3
Austin Lu <austinlu1235@gmail.com>
2025-06-07 19:31:24 -0600

    More work on parser. Need to fix issue with mutable ref to peekable iterator

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -5,29 +5,28 @@ use crate::scanner::token::Token;
 
 mod ast;
 
-struct Parser<'a> {
-    tokens: &'a Vec<Token>,
+struct Parser {
     ast_head : Box<Node>
 }
 
-impl Parser<'_> {
-    pub fn new(tokens : & Vec<Token>) -> Self {
+impl Parser {
+    pub fn new() -> Self {
         Self {
-            tokens : tokens,
             ast_head : Box::new(Node::Empty),
         }
     }
 
-    pub fn gen_ast(&self) {
-        self.ast_head = self.parse(&mut self.tokens.iter().peekable(), 0);
+    pub fn gen_ast(&mut self, tokens : & Vec<Token>) {
+        self.ast_head = self.parse(&mut tokens.iter().peekable(), 0);
     }
 
     // This parser uses pratt parsing, which works somewhat similarly to recursive descent. 
-    fn parse(& self, tok_it : &mut Peekable<Iter<Token>>, min_bp : i32) -> Box<Node> {
+    fn parse(& self, tok_it : &mut Peekable<Iter<Token>>, min_bp : u32) -> Box<Node> {
         // Invariant: The left of the current position of the parser in the token stream has
         // been fully parsed into a single ast.
-        let left = if let Some(x) = tok_it.next() {
-            match x {
+        let mut left = match tok_it.next() {
+            None => panic!("Error, empty token stream"),
+            Some(x) => match x {
                 Token::EOF => Box::new(Node::Empty),
                 Token::IntConst(i) => Box::new(Node::Int(*i)),
                 Token::Id(s) => Box::new(Node::Id{
@@ -36,12 +35,10 @@ impl Parser<'_> {
                             }),
                 x => panic!("Bad token {x:?}"),
             }
-        } else {
-            panic!("Error, empty token stream");
         };
-        // Each iteration, we read some (infix) operator which joins the current left sub tree with
-        // A new, recursively calculated right subtree. We also advance the iterator past the
-        // right subtree, and set the tree with this operator as root.
+        // Each iteration, the iterator is positioned at an (infix) operator which 
+        // will join the current left sub tree with A new, recursively calculated right subtree. 
+        // We also advance the iterator past the right subtree, and set the tree with this operator as root.
         loop {
             let Some(op) = tok_it.peek() else {
                 panic!("Bad token stream: End reached without finding Token::EOF")
@@ -49,12 +46,23 @@ impl Parser<'_> {
             if let Token::EOF = op {
                 break
             }
+            // Return the binding power if op 
             let (lbp, rbp) = self.get_binding_powers(op);
-            
-
+            // The subtree to the left of this is more strongly attracted to the previous operator
+            if lbp < min_bp {
+                break
+            }
+            tok_it.next();
+            // Calculate the right subtree
+            let right = self.parse(tok_it, rbp);
+            left = Box::new(Node::InfixOp {
+                op_type : **op,
+                lhs : left,
+                rhs : right,
+            })
 
         }
-
+        // Return left, which is now the 
         left
     }
 
@@ -68,7 +76,7 @@ impl Parser<'_> {
                 | Token::AddAss 
                 | Token::SubAss 
                 | Token::MulAss 
-                | Token::DivAss => (5, 4), // Right assoc
+                | Token::DivAss => (5, 4), // assignment operators are right associative
             Token::Equal | Token::NotEq => (18, 19),
             Token::GT | Token::GE | Token::LT | Token::LE => (20, 21),
             Token::Add | Token::Sub => (24, 25),
